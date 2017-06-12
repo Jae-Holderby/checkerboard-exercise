4f32daf8eee7de301f21b1a8ae3e3936294eb27f
Jae-Holderby <jae.holderby@gmail.com>
2017-06-12 16:03:33 -0600

    on my way

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Your JS goes here
 
 
+
 function checkerBoard() {
   for (var i = 0; i < window.innerHeight + 1 ; i++) {
     var blackBox = document.createElement('div')
@@ -9,23 +10,14 @@ function checkerBoard() {
     document.body.appendChild(blackBox)
     document.body.appendChild(redBox)
 
-    function getRandomColor() {
-            var lettersNumbers = 'ABCDEF0123456789';
-            var color = '#';
-            for (var i = 0; i < 6; i++ ) {
-                color += lettersNumbers[Math.floor(Math.random() * lettersNumbers.length)];
-            }
-            return color;
-        }
-
     redBox.style.width = "11.1%";
     redBox.style.paddingBottom = "11.1%";
-    redBox.style.backgroundColor = "blue"
+    redBox.style.backgroundImage = "linear-gradient(to bottom, rgba(200, 0, 255, 1) 0%, rgba )"
     redBox.style.float = "left"
 
     blackBox.style.width = "11.1%";
     blackBox.style.paddingBottom = "11.1%";
-    blackBox.style.backgroundColor = "skyblue"
+    blackBox.style.backgroundColor = "rgba(0, 230, 230, 1)"
     blackBox.style.float = "left"
   }
 }
