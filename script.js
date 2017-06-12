cb905bedea081242881808b7f11f4df1c2f666d8
Jae-Holderby <jae.holderby@gmail.com>
2017-06-12 15:37:52 -0600

    Checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your JS goes here
+
+function checkerBoard() {
+  for (var i = 0; i < window.innerHeight + 1 ; i++) {
+    var blackBox = document.createElement('div')
+    var redBox = document.createElement('div')
+
+    document.body.appendChild(blackBox)
+    document.body.appendChild(redBox)
+
+
+    redBox.style.width = "11.1%";
+    redBox.style.paddingBottom = "11.1%";
+    redBox.style.backgroundColor = "black"
+    redBox.style.float = "left"
+
+    blackBox.style.width = "11.1%";
+    blackBox.style.paddingBottom = "11.1%";
+    blackBox.style.backgroundColor = "red"
+    blackBox.style.float = "left"
+  }
+}
+checkerBoard();
