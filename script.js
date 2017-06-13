ea20a8bafa3e62591d5a328e9afe49a83f3ac414
Jae-Holderby <jae.holderby@gmail.com>
2017-06-13 13:34:09 -0600

    better new

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 
 function checkerBoard() {
+  document.body.style.backgroundImage = "linear-gradient( 45deg, white, black )"
   var color1 = 0
   var color2 = 255
   for (var i = 0; i < (window.innerHeight + 1) / 2; i++) {
@@ -14,9 +15,9 @@ function checkerBoard() {
     box.style.paddingBottom = "11.1%";
     box.style.float = "left"
     if (i % 2 === 0) {
-      box.style.backgroundColor = "rgba(0, 0, " + color1 + ", 1)"
+      box.style.backgroundColor = "rgba(0, 0, " + color1 + ", .6)"
     } else {
-      box.style.backgroundColor = "rgba(" + color2 +", 0, 0, 1)"
+      box.style.backgroundColor = "rgba(" + color2 +", 0, 0, .6)"
     }
     color1++
     color2--
